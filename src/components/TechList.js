311666da9fe64fe2d2c72c55c646573ebf250acb
Gustavo <Gustavomgu>
2019-12-11 21:03:02 -0300

    chore: Implementing state in component

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 class TechList extends Component {
+  //Estados são imutaveis, devemos utilizar o this.setState para mudar infos do state
   state = {
+    newTech: '',
     techs: [
       'NodeJS',
       'ReactJS',
@@ -9,13 +11,38 @@ class TechList extends Component {
     ]
   };
 
+  handleInputChange = e => {
+    this.setState({newTech: e.target.value});
+  }
+
+  handleSubmit = e => {
+    // Por padrão o formulario ira recarregar a pagina, para evitar isso usamos o preventDefault();
+    e.preventDefault();
+    // Aqui estou mudando o status, não posso usar um metodo push para adicionar info no array
+    // preciso usar o ... para copiar os dados do array e adicionar a nova info
+    this.setState({ techs: [...this.state.techs, this.state.newTech]});
+    this.state.newTech = '';
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs:
+      this.state.techs.filter(t => t !== tech),
+    });
+  }
+
   render() {
     return (
-      <ul>
-        <li> Node.js </li>
-        <li> ReactJS </li>
-        <li> React Native </li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+        {this.state.techs.map(tech => (
+          <li key={tech}>
+            {tech}
+            <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
+          </li>))}
+        </ul>
+        <input type="text" onChange={this.handleInputChange} value={this.state.newTech}></input>
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
